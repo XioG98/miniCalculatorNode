fe7b86e96511492650bc288634f857e95ace835d
XioG98 <xiogr98@gmail.com>
2025-05-18 11:16:45 -0500

    arreglos

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+//  const { sumar, restar } = require('./OperacionesEnteros/enteros.js')
+//  const { sumarNum, sumarNum } = require('./OperacioneesArreglos')
+
+var ent = require('./OperacionesEnteros/enteros.js');
+const arr = require('./OperacionesArreglos/arreglos.js');
+
 const express = require('express');
 const app = express();
 
-app.listen(3000);
-app.get
+app.get('/quierosumar', (req,res) => {
+    res.end('La suma de 8 y 4 es: '+ ent.sumar(8,4))
+})
+app.listen(3000, () => {
+    console.log("hola")
+});
